463cbeff14398186e22815d8e1abf76bb8588450
Catherine Garner <c16371033@mydit.ie>
2018-03-22 12:37:46 +0000

    Add files via upload

---
+++ ControlAssignment.java
@@ -0,0 +1,9 @@
+
+public class ControlAssignment {
+
+	public static void main(String[] args) 
+	{
+		MainScreen myMainSceen = new MainScreen("My Search Engine");
+	}
+
+}
--- MainScreen.java
+++ MainScreen.java
@@ -15,8 +15,13 @@ public class MainScreen extends JFrame implements ActionListener
 {
 	private JLabel heading;
 	private JPanel Hbackground;
-	private JTextField searchword;
-	private JButton clicksearch;
+	private JTextField searchWord;
+	private JButton clickSearch;
+	private JPanel search;
+	private JTextField chooseText;
+	private JButton clickChoose;
+	private JPanel choose;
+	
 	
 	public MainScreen(String title)
 	{
@@ -33,18 +38,40 @@ public class MainScreen extends JFrame implements ActionListener
 		Hbackground.add(heading);
 		heading.setForeground(Color.WHITE);
 		
-		searchword = new JTextField("Enter search word here");
-		add(searchword);
+		search = new JPanel();
+		add(search);
+		search.setPreferredSize(new Dimension(500,40));
 		
-		clicksearch = new JButton("Search");
-		add(clicksearch);
+		searchWord = new JTextField("Enter search word here");
+		search.add(searchWord);
+		
+		clickSearch = new JButton("Search");
+		clickSearch.addActionListener(this);
+		search.add(clickSearch);
+		
+		choose = new JPanel();
+		add(choose);
+		choose.setPreferredSize(new Dimension(500,40));
+		
+		chooseText = new JTextField("Enter the text you would like to search");
+		choose.add(chooseText);
+		
+		clickChoose = new JButton("Search");
+		clickChoose.addActionListener(this);
+		choose.add(clickChoose);
 		
 		setVisible(true);
 	}
 
 	public void actionPerformed(ActionEvent arg0) 
 	{
+		String text1 = searchWord.getText();
 		
+		if(arg0.getSource()== clickSearch)
+		{
+			
+			
+		}
 	}
 	
 }
