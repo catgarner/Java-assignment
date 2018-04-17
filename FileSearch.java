51af87570a7629eadcb42fd77c8fcba2150c5451
Catherine Garner <c16371033@mydit.ie>
2018-04-17 22:30:52 +0100

    Add files via upload

--- FileSearch.java
+++ FileSearch.java
@@ -24,17 +24,25 @@ public class FileSearch
 	
 	public FileSearch(ArrayList<String> filenames, ArrayList<String> words)
 	{
+		//Instantiating the array lists that were sent in by the main screen class.
 		this.filenames = new ArrayList<String>(filenames);
 		this.words = new ArrayList<String>(words);
 
 	}
 
+	/*
+	 * This method adds the first filename in the filenames array list into a file variable. This 
+	 * variable and the words hash map are sent into the scan file method in the word counter class.
+	 * There is a return variable of an integer. The file name and the count variable that is returned 
+	 * from the scan file method are put into the match hash map. This continues until all the filenames
+	 * in the filenames array list are accounted for. The match hash map is then returned to the main 
+	 * screen class.
+	 */
 	public HashMap<File, Integer> openFile(ArrayList<String> filenames, ArrayList<String> words)
 	{
 		match = new HashMap<File, Integer>();
 		for (int i = 0; i < filenames.size(); i++) 
 		{
-			System.out.println(filenames.get(i));
 			myfile = new File(filenames.get(i));
 			WordCounter counter = new WordCounter(myfile, words);
 			count = counter.scanFile(myfile, words);
